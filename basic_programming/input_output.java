12cfb677b1df1e78894f35c92c0688eccb7ec2e3
Vikash Kumar <vkumar0961@gmail.com>
2020-10-08 11:00:56 +0530

    adding problem

--- basic_programming/input_output.java
+++ basic_programming/input_output.java
@@ -3,11 +3,16 @@ package basic_programming;
 import java.util.Scanner;
 
 public class input_output {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner s = new Scanner(System.in);
+
+        System.out.print("enter your age: ");
+        int age = s.nextInt();
+        s.nextLine();
         System.out.print("enter your name: ");
         String str = s.nextLine();
-        System.out.println("Your name is "+str);
+        System.out.println("Your age is " + age);
+        System.out.println("Your name is " + str);
 
     }
 }
